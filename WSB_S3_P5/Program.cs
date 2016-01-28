460564e2f3efe937888da42cbeda2d1d2df4d884
Pytak <mail@pytak.net>
2016-01-28 06:47:24 +0100

    First release!

--- WSB_S3_P5/Program.cs
+++ WSB_S3_P5/Program.cs
@@ -8,10 +8,15 @@ namespace WSB_S3_P5
 {
     class Program
     {
-        static Tower<Disk>[] Towers = new Tower<Disk>[3];
+        private static Tower<Disk>[] Towers = new Tower<Disk>[3];
+        private static int Score = 0;
+        private static int UserHeight = 0;
+
+        private static int From, To;
+        private static Disk CurrentDisk;
 
 
-        static void InitializeTowers(int TowerHeight)
+        private static void InitializeTowers(int TowerHeight)
         {
             for (int i = 0; i < 3; i++)
             {
@@ -24,8 +29,8 @@ namespace WSB_S3_P5
             }
         }
 
-        static void DrawTowers()
-        {
+        private static void DrawTowers()
+        {   
             Console.CursorTop = 0;
             for (int i = 0; i < 3; i++)
             {
@@ -34,12 +39,19 @@ namespace WSB_S3_P5
             }
         }
 
+        private static void DrawScore()
+        {
+            Console.CursorLeft = 0;
+            Console.CursorTop = UserHeight + 1;
+            Console.WriteLine("Ruchy: " + Score);
+        }
+
         private static int CheckRangeFromKB(int Min, int Max)
         {
             int ValidInt;
             while (true)
             {
-                if (int.TryParse(Console.ReadKey().KeyChar.ToString(), out ValidInt))
+                if (int.TryParse(Console.ReadKey(true).KeyChar.ToString(), out ValidInt))
                 {
                     if (ValidInt >= Min && ValidInt <= Max)
                     {
@@ -47,7 +59,7 @@ namespace WSB_S3_P5
                     }
                 }
                 Console.Write(" (" + Min + "-" + Max + ")  ");
-                Console.CursorLeft -= 9;
+                Console.CursorLeft -= 8;
             }
             return ValidInt;
         }
@@ -59,37 +71,61 @@ namespace WSB_S3_P5
 
 
             Console.Write("Podaj wysokość wieży: ");
-            int UserHeight = CheckRangeFromKB(3, 9);
-
+            UserHeight = CheckRangeFromKB(3, 9);
+            Console.Clear();
 
             InitializeTowers(UserHeight);
 
 
-            int Next;
-            Disk CurrentDisk;
+            
             while (true)
             {
-                Console.Clear();
                 DrawTowers();
+                DrawScore();
+
+                if (Towers[2].Height == UserHeight)
+                {
+                    break;
+                }
 
                 Console.CursorTop = UserHeight;
 
                 Console.Write("Przenieś z:  ");
-                Next = CheckRangeFromKB(1, 3);
+                From = CheckRangeFromKB(1, 3);
 
-                CurrentDisk = Towers[Next - 1].Pop();
+                try { CurrentDisk = Towers[From - 1].Pop(); }
+                catch (EmptyTowerException)
+                {
+                    Console.Write("(Pusto!)");
+                    continue;
+                }
+                finally
+                {
+                    Console.Write("        ");
+                }
 
                 Console.CursorLeft = 0;
 
                 Console.Write("Przenieś do: ");
-                Next = CheckRangeFromKB(1, 3);
+                To = CheckRangeFromKB(1, 3);
 
-                Towers[Next - 1].Push(CurrentDisk);
+                try { Towers[To - 1].Push(CurrentDisk); }
+                catch (LargerOnSmallerElementException)
+                {
+                    Console.Write("(Źle!)");
+                    Towers[From - 1].Push(CurrentDisk);
+                    continue;
+                }
+
+                Score++;
+
+                Console.Clear();
             }
 
+            Console.Write("Udało się! Naciśnij dowolny klawisz, by wyjść.");
 
-            Console.ReadLine();
-        
+            Console.ReadKey();
+
         }
 
     }
